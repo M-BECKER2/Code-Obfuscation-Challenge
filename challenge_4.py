71f9e7476ab7ec38da0245df366fe5142cc4a7a3
Madelyn <163598094+M-BECKER2@users.noreply.github.com>
2024-03-28 16:00:44 -0600

    Update challenge_4.py
    fixed some spelling

--- challenge_4.py
+++ challenge_4.py
@@ -1,30 +1,30 @@
 import base64
 
-var1 = "secert"
+var1 = "secret"
 var2 = "notthis"
 var3 = "hellothere"
 var4 = "bonjour"
 
 def base(a): 
-    secert = "MTE1LDEwMSw5OSwxMDEsMTE0LDExNg=="    
+    secret = "MTE1LDEwMSw5OSwxMDEsMTE0LDExNg=="    
     num = ','.join(str(ord(c)) for c in secert)
     enc = num.encode('ascii')
     base2 = base64.b64encode(enc)
     enc2= base2.decode('ascii')
     return enc2
 def base2(a):
-    secert = "bm90dGhpcw=="
-    enc = secert.encode('ascii')
+    secret = "bm90dGhpcw=="
+    enc = secret.encode('ascii')
     base2 = base64.b64encode(enc)
     encrypt= base2.decode('ascii')
     return encrypt
 def rot13(a):
     boo = "abcdefghijklmnopqrstuvwxyz"
-    secert = "uryybgurer" 
+    secret = "uryybgurer" 
     encrypt = "".join([boo[(boo.find(c)+13)%26] for c in a])
     return encrypt
 def xor_strings(s) -> bytes:
-    secert = "@MLHMWP" 
+    secret = "@MLHMWP" 
     return "".join(chr(ord(a) ^ b) for a, b in zip(s, t)).encode('utf8')
 def toBinary(a):
   l,m=[],[]
@@ -42,12 +42,12 @@ if (user_input[0]) == 's':
     userinput = rot13(userinput)
     if userinput == var1:
         userinput = base(userinput)
-        print("That is not the correct secert")
+        print("That is not the correct secret")
     else:
-        print("That is not the correct secert")
+        print("That is not the correct secret")
 else:
     if user_input[0] == "m":
-        print("That is not the correct secert")
+        print("That is not the correct secret")
     elif user_input[0] == "n":
         var2 = base2(var2)
         userinput = rot13(user_input)
@@ -55,15 +55,15 @@ else:
             userinput = rot13(userinput)
             userinput = base2(userinput) 
             if userinput == var2:
-                print("This is the correct secert")
+                print("This is the correct secret")
             else:
                 userinput = rot13(user_input)
-                print("This is not the correct secert")
+                print("This is not the correct secret")
         else:
             if userinput == var4:
                 userinput = base2(userinput)
-                print("This is not the correct secert")
+                print("This is not the correct secret")
             else:
-                print("This is not the correct secert")
+                print("This is not the correct secret")
     else:
-        print("This is not the correct secert")
+        print("This is not the correct secret")
